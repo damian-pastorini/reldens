cc463599665c0a268d6f3e5d1c6a2631297fe937
Damian A. Pastorini <damian.pastorini>
2018-08-07 22:27:51 -0400

    - App v1.0.0

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(process.env.PORT || 2000);
+console.log("Server started.");
