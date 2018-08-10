428290b20e5d518096587e4c5ef4a9b7572546e9
Damian A. Pastorini <damian.pastorini>
2018-08-10 18:44:34 -0400

    - Login form

---
+++ client/js/login.js
@@ -0,0 +1,9 @@
+/**
+ * Initialize!
+ */
+$( document ).ready(function() {
+    console.log('ready!');
+    $('login-form').on('submit', function() {
+        console.log('Logging...');
+    });
+});
