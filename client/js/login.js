1394363b3fd6958f05a83440f14fb5ccb7075e07
Damian A. Pastorini <damian.pastorini>
2018-08-10 20:51:35 -0400

    - Login emit

--- client/js/login.js
+++ client/js/login.js
@@ -1,12 +1,15 @@
 /**
  * Initialize!
  */
+var socket = io();
+// wait for doc and start:
 $(document).ready(function() {
     console.log('ready!');
     if($('#login-form').length) {
         $('#login-form').on('submit', function(e){
             console.log('Logging...');
             e.preventDefault();
+            socket.emit('signIn', {username: $('#username').val(), password: $('#password').val()});
         });
     }
 });
