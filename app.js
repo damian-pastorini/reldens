030892194bee780ef0edb11ba0a636b8ff02f7eb
Damian A. Pastorini <damian.pastorini>
2018-08-10 20:50:23 -0400

    - Implemented Unique ID for sessions

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+// server configuration:
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-
-app.get('/',function(req, res) {
+// client configuration:
+app.get('/', function(req, res){
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
-
+// server start:
 serv.listen(process.env.PORT || 8080);
-
 console.log('Server started.');
+
+var SOCKET_LIST = {};
+var io = require('socket.io')(serv, {});
+
+var uniqid = require('uniqid');
+
+io.sockets.on('connection', function(socket){
+    socket.id = uniqid();
+    SOCKET_LIST[socket.id] = socket;
+    socket.on('disconnect', function(){
+        delete SOCKET_LIST[socket.id];
+    });
+    socket.on('signIn', function(data){
+		console.log(data);
+    });
+});
