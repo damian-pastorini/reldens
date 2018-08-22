e4d5c08304f8f839651f8a36da742c946d6ad532
Damian A. Pastorini <damian.pastorini>
2018-08-22 18:24:33 -0300

    - Implemented promise-mysql in Link class.

--- server/modules/link.js
+++ server/modules/link.js
@@ -1,61 +1,17 @@
-class Link
-{
+var mysql = require('promise-mysql');
 
-    constructor()
-    {
-        this.mysql = false;
-        this.link = false;
-    }
+pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin22',
+    database: 'questworld',
+    connectionLimit: 10
+});
 
-    getConnection()
-    {
-        if(!this.link){
-            if(this.mysql){
-                this.link = this.mysql.createConnection({
-                    host: "localhost",
-                    user: "root", // root
-                    password: "admin22", // admin22
-                    database: 'questworld'
-                });
-            } else {
-                // error.
-            }
-        }
-        return this.link;
-    }
-
-    connect()
-    {
-        var con = this.getConnection();
-        return con.connect(function(err){
-            if(err){
-                throw err;
-            }
-        });
-    }
-
-    select(sql, callback, originalParams)
-    {
-        var con = this.getConnection();
-        con.query(sql, function(err, result){
-            if(err){
-                throw err;
-            }
-            callback(err, result, originalParams);
-        });
-    }
-
-    insert(table, values, callback, originalParams)
-    {
-        var con = this.getConnection();
-        con.query('INSERT INTO '+table+' VALUES('+values+')', function(err, result) {
-            if(err){
-                throw err;
-            }
-            callback(result, originalParams);
-        });
-    }
-
+function getSqlConnection() {
+    return pool.getConnection().disposer(function(connection) {
+        pool.releaseConnection(connection);
+    });
 }
 
-exports.link = Link;
+module.exports = getSqlConnection;
