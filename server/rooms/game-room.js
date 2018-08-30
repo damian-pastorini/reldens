d6c95089378028c5ed32758498d5ff0757db9272
Damian A. Pastorini <damian.pastorini>
2018-08-30 19:20:09 -0300

    - QuestWorld v1.1.0 - Colyseus + MySQL + Login and registration

--- server/rooms/game-room.js
+++ server/rooms/game-room.js
@@ -1,33 +1,71 @@
 var Room = require('colyseus').Room;
 var State = require('../modules/state').state;
+var DbLink = require('../modules/datalink');
 
 class GameRoom extends Room
 {
 
     onInit(options)
     {
-        console.log('GameRoom created!', options);
+        // console.log('GameRoom created!', options);
         this.setState(new State());
     }
 
-    onJoin(client)
+    onAuth(options)
     {
-        console.log('join session:'+client.sessionId);
-        console.log(this.hasReachedMaxClients());
-        this.state.createPlayer(client.sessionId);
+        // console.log('onAuth: ', options);
+        if(options.isNewUser){
+            // registration (default role_id = 1, status = 1, state = 1):
+            var queryString = 'INSERT INTO users VALUES(NULL, "'+options.email+'", "'+options.username+'", "'+options.password+'", 1, 1, 1);';
+        } else {
+            // login:
+            var queryString = 'SELECT * FROM users WHERE username="'+options.username+'" AND password="'+options.password+'";';
+        }
+        // console.log(queryString);
+        return new Promise((resolve, reject) => {
+            DbLink.connection.query(queryString, options, (err, rows) => {
+                if(err){
+                    return reject(options);
+                }
+                if(rows){
+                    if(options.isNewUser){
+                        options.isNewUser = false;
+                        options.role_id = 1;
+                        options.status = 1;
+                        options.state = 1;
+                        resolve(options);
+                    } else {
+                        // temporal fix to get row data values as object:
+                        var currentPlayer = '';
+                        for(var i=0; i<rows.length; i++){
+                            currentPlayer = rows[i];
+                            break;
+                        }
+                        // console.log(currentPlayer);
+                        resolve(currentPlayer);
+                    }
+                }
+            });
+        });
+    }
+
+    onJoin(client, options, auth)
+    {
+        // console.log(this.hasReachedMaxClients());
+        // console.log('session / options / auth: ', client.sessionId, options, auth);
+        this.state.createPlayer(client.sessionId, auth);
     }
 
     onLeave(client)
     {
-        console.log('leave session:'+client.sessionId);
-        console.log(this.hasReachedMaxClients());
+        // console.log('Leave session: '+client.sessionId);
         this.state.removePlayer(client.sessionId);
     }
 
     onMessage(client, data)
     {
-        console.log('GameRoom received message from', client.sessionId, ':', data);
         this.state.movePlayer(client.sessionId, data);
+        // console.log('Message:', client.sessionId, ':', data);
     }
 
     onDispose()
