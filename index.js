36e0cdd48ce65535e8ce84046228b8914a86ce80
Damian A. Pastorini <damian.pastorini>
2018-09-05 23:28:25 -0300

    - Included Phaser 3 modules and modified start script. - Included Parcel bundle. - Moved server main file.

--- index.js
+++ index.js
@@ -1,25 +1,2 @@
-const colyseus = require('colyseus');
-const http = require('http');
 
-const express = require('express');
-const app = express();
-const port = Number(process.env.PORT || 8080);
-
-const server = http.createServer(app);
-const gameServer = new colyseus.Server({server: server});
-gameServer.register('game_room', require('./server/rooms/game-room').gameroom);
-
-const path = require('path');
-const monitor = require('@colyseus/monitor');
-
-app.use('/', express.static(path.join(__dirname, 'client')));
-
-// (optional) attach web monitoring panel
-app.use('/colyseus', monitor.monitor(gameServer));
-
-gameServer.onShutdown(function(){
-  console.log('Game Server is going down.');
-});
-
-gameServer.listen(port);
-console.log('Listening on http://localhost:'+port);
+module.exports = require('./server/index');
