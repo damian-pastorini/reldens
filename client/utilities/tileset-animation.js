e60a7b331453aaaf267a21deb071df7857078dcf
Damian A. Pastorini <damian.pastorini>
2018-09-16 20:40:13 -0300

    [9]- Format changes.

--- client/utilities/tileset-animation.js
+++ client/utilities/tileset-animation.js
@@ -12,7 +12,7 @@ class TilesetAnimation
     {
         this.layer = layer;
         let index = 0;
-        for (let i in tileData) {
+        for(let i in tileData){
             let tile = tileData[i];
             tile.id = index++;
             tile.init = i;
@@ -22,25 +22,25 @@ class TilesetAnimation
 
     start()
     {
-        for (let anm of this.animations) {
+        for(let anm of this.animations){
             this.repeat(anm.id, anm.animation, anm.init, 0);
         }
     }
 
     repeat(id, animation, prev, index)
     {
-        if (this.registered[id]){
+        if(this.registered[id]){
             this.registered[id] = null;
         }
-        this.layer.replaceByIndex(prev, animation[index].tileid + 1);
+        this.layer.replaceByIndex(prev, animation[index].tileid+1);
         let total = animation.length;
         this.registered[id] = setTimeout(this.repeat.bind(this, id, animation, animation[index % total].tileid + 1, (index + 1) % total), animation[index].duration);
     }
 
     destroy()
     {
-        for (let i in this.registered) {
-            if (this.registered[i]) {
+        for(let i in this.registered){
+            if(this.registered[i]){
                 clearTimeout(this.registered[i]);
             }
         }
