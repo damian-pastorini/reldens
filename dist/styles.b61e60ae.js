f728c6729dc620bc3e13f3b84f44b6303147ec37
Damian A. Pastorini <damian.pastorini>
2019-10-30 00:09:50 +0100

    - Reldens - v4.0.0 - Updated dist.

--- dist/styles.b61e60ae.js
+++ dist/styles.b61e60ae.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35957" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37749" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
