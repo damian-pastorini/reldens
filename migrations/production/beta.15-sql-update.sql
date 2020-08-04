8edaba443e17470848602b3b20ba66e08edfcc33
Damian A. Pastorini <damian.pastorini>
2020-08-04 23:03:01 +0200

    - Reldens - v4.0.0 - Updated items system.

--- migrations/production/beta.15-sql-update.sql
+++ migrations/production/beta.15-sql-update.sql
@@ -6,10 +6,11 @@ ALTER TABLE `users` CHANGE COLUMN `status` `status` VARCHAR(255) NOT NULL AFTER 
 
 INSERT INTO `reldens`.`features` (`code`, `title`, `is_enabled`) VALUES ('firebase', 'Firebase', '1');
 
-# Missing key on modifiers table:
+# Missing key and fixed operation field type in modifiers table:
 
 ALTER TABLE `items_item_modifiers` ADD COLUMN `key` VARCHAR(255) NOT NULL AFTER `item_id`;
 ALTER TABLE `items_item_modifiers` ADD COLUMN `maxProperty` VARCHAR(255) NULL DEFAULT NULL AFTER `value`;
+ALTER TABLE `items_item_modifiers` CHANGE COLUMN `operation` `operation` INT(11) NOT NULL COLLATE 'utf8_unicode_ci' AFTER `property_key`;
 
 INSERT INTO `items_item_modifiers` (`id`, `item_id`, `key`, `property_key`, `operation`, `value`, `maxProperty`) VALUES (1, 4, 'atk', 'stats/atk', '5', '5', NULL);
 INSERT INTO `items_item_modifiers` (`id`, `item_id`, `key`, `property_key`, `operation`, `value`, `maxProperty`) VALUES (3, 5, 'atk', 'stats/atk', '5', '3', NULL);
