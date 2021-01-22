66f1d33deb3d1552977d46004a7b62a399dfd8e7
Damian A. Pastorini <damian.pastorini>
2021-01-23 00:29:11 +0100

    - Reldens - v4.0.0 - Fixed bug - Bullet hit moving the player.

--- migrations/production/beta.17-sql-update.sql
+++ migrations/production/beta.17-sql-update.sql
@@ -7,6 +7,7 @@ SET FOREIGN_KEY_CHECKS=0;
 
 UPDATE `config` SET `value`=0 WHERE `path`='ui/chat/defaultOpen';
 INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'players/animations/collideWorldBounds', '1', 'b');
+INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('server', 'rooms/world/bulletsStopOnPlayer', '1', 'b');
 
 #######################################################################################################################
 
