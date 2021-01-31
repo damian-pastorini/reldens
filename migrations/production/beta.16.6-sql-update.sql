5f0875333bd86b13d6772524ba5225b5c80df405
Damian A. Pastorini <damian.pastorini>
2021-01-31 23:21:30 +0100

    - Reldens - v4.0.0 - Update config.

--- migrations/production/beta.16.6-sql-update.sql
+++ migrations/production/beta.16.6-sql-update.sql
@@ -7,6 +7,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 # Config:
 
 INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'players/animations/fallbackImage', 'player-base', 't');
+UPDATE `config` SET `value`='' WHERE  `path`= 'ui/controls/defaultActionKey';
 
 # Update skills system:
 
