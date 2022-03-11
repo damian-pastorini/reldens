055933a51790e5c176f7eb48e221c3f061e64d20
Damian A. Pastorini <damian.pastorini@gmail.com>
2022-03-11 13:50:28 +0100

    - Reldens - v4.0.0 - Fixed migration version. - Included new chat options config.

--- migrations/production/beta.21-sql-update.sql
+++ migrations/production/beta.21-sql-update.sql
@@ -5,7 +5,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 #######################################################################################################################
 
 # Config:
+
 INSERT INTO config VALUES(NULL, 'client', 'players/tapMovement/enabled', '1', 'b');
+
+# Chat:
+
+INSERT INTO config VALUES(NULL, 'client', 'ui/chat/useOverheadChat', '1', 'b');
+INSERT INTO config VALUES(NULL, 'client', 'chat/messages/characterLimit', '100', 'i');
+INSERT INTO config VALUES(NULL, 'client', 'chat/messages/characterLimitOverhead', '50', 'i');
 
 #######################################################################################################################
 
