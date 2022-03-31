faf8574e374c68be09a0a957f03b71e5ecbce852
Damian A. Pastorini <damian.pastorini@gmail.com>
2022-03-31 19:03:00 +0200

    - Reldens - v4.0.0 - PvP battle end fix.

--- migrations/production/beta.23-sql-update.sql
+++ migrations/production/beta.23-sql-update.sql
@@ -6,6 +6,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 # Config:
 UPDATE `config` SET `value` = 25 WHERE `path` LIKE 'enemies/initialStats/%' AND `value` = 10;
+UPDATE `config` SET `value` = 0 WHERE `path` = 'ui/controls/primaryMove';
 
 #######################################################################################################################
 
