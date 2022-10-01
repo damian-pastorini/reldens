1723e6c5774da48b4b92734e209bef6e2bb57195
Damian A. Pastorini <damian.pastorini@gmail.com>
2022-10-01 21:31:15 +0200

    - Reldens - v4.0.0 - WIP trade message listener and templates.

--- theme/plugins/server-plugin.js
+++ theme/plugins/server-plugin.js
@@ -6,9 +6,9 @@
 
 const { PluginInterface } = require('reldens/lib/features/plugin-interface');
 const { NpcObject } = require('reldens/lib/objects/server/object/type/npc-object')
+const { TraderObject } = require('reldens/lib/objects/server/object/type/trader-object');
 const { Door } = require('./objects/server/door');
 const { Healer } = require('./objects/server/healer');
-const { Merchant } = require('./objects/server/merchant');
 const { QuestNpc } = require('./objects/server/quest-npc');
 const { WeaponsMaster } = require('./objects/server/weapons-master');
 const { Enemy1 } = require('./objects/server/enemy1');
@@ -40,7 +40,7 @@ class ServerPlugin extends PluginInterface
         customClasses.objects['door_2'] = Door;
         customClasses.objects['npc_1'] = NpcObject;
         customClasses.objects['npc_2'] = Healer;
-        customClasses.objects['npc_3'] = Merchant;
+        customClasses.objects['npc_3'] = TraderObject;
         customClasses.objects['npc_4'] = WeaponsMaster;
         customClasses.objects['npc_5'] = QuestNpc;
         customClasses.objects['enemy_1'] = Enemy1;
