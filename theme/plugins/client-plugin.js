388d11fa63f5117e2608d23711e128ba6901c532
Lucio <luchovicentini@gmail.com>
2022-10-09 22:09:55 +0200

    - Reldens - v4.0.0 - EngineDriver - Removed Phaser's Game import from game-engine.js.

--- theme/plugins/client-plugin.js
+++ theme/plugins/client-plugin.js
@@ -7,6 +7,7 @@
 const { PluginInterface } = require('reldens/lib/features/plugin-interface');
 const { Npc1 } = require('./objects/client/npc1');
 const { sc } = require('@reldens/utils');
+const { PhaserDriver } = require('reldens/lib/game/client/engine/phaser-driver')
 
 class ClientPlugin extends PluginInterface
 {
@@ -17,6 +18,9 @@ class ClientPlugin extends PluginInterface
         this.events.on('reldens.beforeJoinGame', (props) => {
             this.defineCustomClasses(props);
         });
+        this.events.on('reldens.beforeCreateEngine', (props) => {
+            this.defineEngineDriver(props);
+        })
     }
 
     defineCustomClasses(props)
@@ -28,6 +32,12 @@ class ClientPlugin extends PluginInterface
         customClasses.objects['people_town_1'] = Npc1;
     }
 
+    defineEngineDriver(props)
+    {
+        if ('phaser' === props.gameConfig.engine) {
+            props.gameConfig.client['engineDriver'] = new PhaserDriver();
+        }
+    }
 }
 
 module.exports.ClientPlugin = ClientPlugin;
