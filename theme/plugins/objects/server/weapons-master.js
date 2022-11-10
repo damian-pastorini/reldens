dec8eabb71d5d40bc1878b9ccf5588a5e5a8296d
Damian A. Pastorini <damian.pastorini@gmail.com>
2022-11-10 02:18:24 +0100

    - Reldens - v4.0.0 - WIP trader requirements and rewards.

--- theme/plugins/objects/server/weapons-master.js
+++ theme/plugins/objects/server/weapons-master.js
@@ -16,12 +16,11 @@ class WeaponsMaster extends NpcObject
 
     async executeMessageActions(client, data, room, playerSchema)
     {
-        await super.executeMessageActions(client, data, room, playerSchema);
-        let optionIdx = 'op'+data.value;
-        if(!this.isValidOption(data) || !this.isValidIndexValue(optionIdx, room, client)){
+        let superResult = await super.executeMessageActions(client, data, room, playerSchema);
+        if(false === superResult){
             return false;
         }
-        let selectedOption = this.options[optionIdx];
+        let selectedOption = this.options[data.value];
         if(sc.hasOwn(playerSchema.inventory.manager.items, selectedOption.key)){
             let contentMessage = 'You already have the item.';
             client.send('*', {act: GameConst.UI, id: this.id, content: contentMessage});
