b63f1f027ce4f5192466772889d6788c117409b2
Lucio Vicentini <luchovicentini@gmail.com>
2023-01-31 14:57:34 +0100

    Deleted enemy giving a branch as default behaviour. Refactor rewards code. Added chat messages for rewards.

--- theme/plugins/objects/server/enemy1-object.js
+++ theme/plugins/objects/server/enemy1-object.js
@@ -5,7 +5,7 @@
  */
 
 const { EnemyObject } = require('reldens/lib/objects/server/object/type/enemy-object');
-const { Logger, sc } = require('@reldens/utils');
+const { sc } = require('@reldens/utils');
 
 class Enemy1Object extends EnemyObject
 {
@@ -19,11 +19,6 @@ class Enemy1Object extends EnemyObject
         // @TODO - BETA - Rewards as items or experience will be coming from the storage.
         if(sc.hasOwn(playerSchema, 'skillsServer')){
             await playerSchema.skillsServer.classPath.addExperience(50);
-        }
-        let treeBranch = playerSchema.inventory.manager.createItemInstance('branch');
-        let addResult = await playerSchema.inventory.manager.addItem(treeBranch);
-        if(false === addResult){
-            Logger.error(['Error while adding item "branch".']);
         }
     }
 
