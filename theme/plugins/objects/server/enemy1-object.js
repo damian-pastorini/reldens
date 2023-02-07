143fa478409a65b5d451643cc7041a947ae7fd1d
Lucio Vicentini <luchovicentini@gmail.com>
2023-02-07 11:14:27 +0100

    code refactor and minor bug fixing.

--- theme/plugins/objects/server/enemy1-object.js
+++ theme/plugins/objects/server/enemy1-object.js
@@ -12,14 +12,15 @@ class Enemy1Object extends EnemyObject
 
     async onBattleEnd(playerSchema, pveInstance, actionData)
     {
+        /*
         // validate unique id for battle end event:
         if(this.uid !== pveInstance.targetObject.uid){
             return false;
         }
-        // @TODO - BETA - Rewards as items or experience will be coming from the storage.
         if(sc.hasOwn(playerSchema, 'skillsServer')){
             await playerSchema.skillsServer.classPath.addExperience(50);
         }
+        */
     }
 
 }
