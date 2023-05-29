eb716dc7cde63c12d06a935e549366fef66e5dd7
Damian A. Pastorini <damian.pastorini@gmail.com>
2023-05-29 15:59:17 +0200

    - Reldens - v4.0.0 - Snippets WIP.

--- migrations/production/beta.26.3-sql-update.sql
+++ migrations/production/beta.26.3-sql-update.sql
@@ -16,6 +16,7 @@ CREATE TABLE `locale` (
 	`locale` VARCHAR(5) NOT NULL COLLATE 'utf8mb4_unicode_ci',
 	`language_code` VARCHAR(2) NOT NULL COLLATE 'utf8mb4_unicode_ci',
 	`country_code` VARCHAR(2) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+	`enabled` INT(10) UNSIGNED NOT NULL DEFAULT '1',
 	PRIMARY KEY (`id`) USING BTREE
 ) COLLATE='utf8mb4_unicode_ci' ENGINE=InnoDB;
 
@@ -27,6 +28,18 @@ CREATE TABLE `snippets` (
 	PRIMARY KEY (`id`) USING BTREE,
 	INDEX `locale_id` (`locale_id`) USING BTREE,
 	CONSTRAINT `FK_snippets_locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+) COLLATE='utf8mb4_unicode_ci' ENGINE=InnoDB;
+
+CREATE TABLE `players_locale` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`locale_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+	`player_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `locale_id_player_id` (`locale_id`, `player_id`) USING BTREE,
+	INDEX `locale_id` (`locale_id`) USING BTREE,
+	INDEX `player_id` (`player_id`) USING BTREE,
+	CONSTRAINT `FK_players_locale_locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `FK_players_locale_players` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 ) COLLATE='utf8mb4_unicode_ci' ENGINE=InnoDB;
 
 INSERT INTO `locale` VALUES (1, 'en_US', 'en', 'US');
