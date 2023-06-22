e94f8175694a8d6127fd5ab749c340967fb3bec8
Damian A. Pastorini <damian.pastorini@gmail.com>
2023-06-22 19:58:33 +0200

    - Reldens - v4.0.0 - Snippets WIP.

--- migrations/production/beta.26.3-sql-update.sql
+++ migrations/production/beta.26.3-sql-update.sql
@@ -45,8 +45,9 @@ CREATE TABLE `players_locale` (
 INSERT INTO `locale` VALUES (1, 'en_US', 'en', 'US');
 
 # Features:
+ALTER TABLE `features` ADD UNIQUE INDEX `code` (`code`);
+
 INSERT INTO `features` VALUES (NULL, 'snippets', 'Snippets', 1);
-INSERT INTO `features` VALUES (NULL, 'objects', 'Objects', 1);
 
 # Chat UI:
 CREATE TABLE `chat_message_types` (
--- theme/plugins/server-plugin.js
+++ theme/plugins/server-plugin.js
@@ -5,16 +5,13 @@
  */
 
 const { PluginInterface } = require('reldens/lib/features/plugin-interface');
-const { NpcObject } = require('reldens/lib/objects/server/object/type/npc-object');
-const { TraderObject } = require('reldens/lib/objects/server/object/type/trader-object');
-const { DropObject } = require('reldens/lib/objects/server/object/type/drop-object');
-const { Door } = require('./objects/server/door');
+// const { DropObject } = require('reldens/lib/objects/server/object/type/drop-object');
 const { Healer } = require('./objects/server/healer');
 const { QuestNpc } = require('./objects/server/quest-npc');
 const { WeaponsMaster } = require('./objects/server/weapons-master');
-const { Enemy1 } = require('./objects/server/enemy1');
-const { Enemy2 } = require('./objects/server/enemy2');
-const { ObjectsConst } = require('reldens/lib/objects/constants');
+//const { Enemy1 } = require('./objects/server/enemy1');
+//const { Enemy2 } = require('./objects/server/enemy2');
+//const { ObjectsConst } = require('reldens/lib/objects/constants');
 const { sc } = require('@reldens/utils');
 
 class ServerPlugin extends PluginInterface
@@ -39,16 +36,12 @@ class ServerPlugin extends PluginInterface
         }
         // @TODO - BETA - Clean up all the custom classes, by default these can be all default objects with all the
         //   data coming from the storage. Leave just a custom class as sample like the "Npc1" on the client-plugin.
-        // customClasses.objects['door_1'] = Door;
-        // customClasses.objects['door_2'] = Door;
-        customClasses.objects['npc_1'] = NpcObject;
         customClasses.objects['npc_2'] = Healer;
-        customClasses.objects['npc_3'] = TraderObject;
         customClasses.objects['npc_4'] = WeaponsMaster;
         customClasses.objects['npc_5'] = QuestNpc;
-        customClasses.objects['enemy_1'] = Enemy1;
-        customClasses.objects['enemy_2'] = Enemy2;
-        customClasses.objects[ObjectsConst.TYPE_DROP] = DropObject;
+        // customClasses.objects['enemy_1'] = Enemy1;
+        // customClasses.objects['enemy_2'] = Enemy2;
+        // customClasses.objects[ObjectsConst.TYPE_DROP] = DropObject;
     }
 
 }
