104ff1955d24af34e4b7ff0d3b732a7d2f3b9fda
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-03-14 21:32:52 +0100

    - Reldens - v4.0.0 - Import commands and classes.

--- bin/import.js
+++ bin/import.js
@@ -2,6 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
+const { ServerManager } = require('../server');
+const { PlayersExperiencePerLevelImporter } = require('../lib/import/server/players-experience-per-level-importer');
+const { MonstersExperiencePerLevelImporter } = require('../lib/import/server/monsters-experience-per-level-importer');
+const { AttributesPerLevelImporter } = require('../lib/import/server/attributes-per-level-importer');
+const { ClassPathsImporter } = require('../lib/import/server/class-paths-importer');
 
 let args = process.argv;
 if(2 === args.length){
@@ -10,13 +15,57 @@ if(2 === args.length){
 }
 
 let validCommands = {
-    'players-experience-per-level': (data) => {
+    'players-experience-per-level': async (data, projectThemeName) => {
+        let serverManager = await initializeServer(data, projectThemeName);
+        if (!serverManager) {
+            return false;
+        }
+        let importer = new PlayersExperiencePerLevelImporter(serverManager);
+        await importer.import(data);
     },
-    'monsters-experience-per-level': (data) => {
+    'monsters-experience-per-level': async (data, projectThemeName) => {
+        let serverManager = await initializeServer(data, projectThemeName);
+        if (!serverManager) {
+            return false;
+        }
+        let importer = new MonstersExperiencePerLevelImporter(serverManager);
+        await importer.import(data);
     },
-    'attributes-per-level': (data) => {
-    }
+    'attributes-per-level': async (data, projectThemeName) => {
+        let serverManager = await initializeServer(data, projectThemeName);
+        if (!serverManager) {
+            return false;
+        }
+        let importer = new AttributesPerLevelImporter(serverManager);
+        await importer.import(data);
+    },
+    'class-paths': async (data, projectThemeName) => {
+        let serverManager = await initializeServer(data, projectThemeName);
+        if (!serverManager) {
+            return false;
+        }
+        let importer = new ClassPathsImporter(serverManager);
+        await importer.import(data);
+    },
 };
+
+async function initializeServer(data, projectThemeName)
+{
+    if (!data) {
+        console.error('- Missing data.', data);
+        return false;
+    }
+    let appServer = new ServerManager({
+        projectRoot: process.cwd(),
+        projectThemeName
+    });
+    if (!appServer) {
+        console.error('- Can not initialize server.', projectThemeName);
+        return false;
+    }
+    await appServer.initializeStorage(appServer.rawConfig, appServer.dataServerDriver);
+    return appServer;
+}
 
 function fetchFileContents(filePath)
 {
@@ -54,9 +103,21 @@ if (!command) {
     return false;
 }
 
+let themeName = extractedParams[1] || '';
+if (!themeName) {
+    console.error('- Missing active theme name.');
+    return false;
+}
+
 if (-1 === Object.keys(validCommands).indexOf(command)) {
     console.error('- Invalid command.', command);
     return false;
 }
 
-validCommands[command](fetchFileContents(extractedParams[1] || ''));
+validCommands[command](fetchFileContents(extractedParams[2] || ''), themeName).then(() => {
+    console.log('Done.');
+    process.exit();
+}).catch((error) => {
+    console.error(error);
+    process.exit();
+});
