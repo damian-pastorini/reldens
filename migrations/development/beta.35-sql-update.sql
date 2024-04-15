076fe6de995353b8b115d7dce5a1294e1fc8ce5a
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-04-15 15:33:17 +0200

    - Reldens - v4.0.0 - Removed old closeInventoryOnUse from sample data.

--- migrations/development/beta.35-sql-update.sql
+++ migrations/development/beta.35-sql-update.sql
@@ -7,6 +7,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 # Rewards assets fix:
 UPDATE `objects_items_rewards_animations` SET `file` = CONCAT(`file`, '.png') WHERE `file` NOT LIKE '%.png';
 
+# Items close inventory fix:
+UPDATE items_item SET customData = JSON_REMOVE(customData, '$.animationData.closeInventoryOnUse') WHERE JSON_CONTAINS(customData, '{"animationData":{"closeInventoryOnUse":true}}');
+
 #######################################################################################################################
 
 SET FOREIGN_KEY_CHECKS = 1;
