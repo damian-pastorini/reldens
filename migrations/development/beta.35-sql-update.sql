16699012887289184851276d9565e841499c1fa6
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-04-17 17:43:47 +0200

    - Reldens - v4.0.0 - Allow and control guest login.

--- migrations/development/beta.35-sql-update.sql
+++ migrations/development/beta.35-sql-update.sql
@@ -4,6 +4,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 #######################################################################################################################
 
+# Config:
+INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'general/users/allowRegistration', '1', 3);
+INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'general/users/allowGuest', '1', 3);
+INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('server', 'players/guestUser/roleId', '2', 2);
+
 # Rewards assets fix:
 UPDATE `objects_items_rewards_animations` SET `file` = CONCAT(`file`, '.png') WHERE `file` NOT LIKE '%.png';
 
