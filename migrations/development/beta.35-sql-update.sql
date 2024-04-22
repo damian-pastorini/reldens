1b31deea8bfc27a272235b9a16b0a3b9e8360cd3
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-04-22 20:18:59 +0200

    - Reldens - v4.0.0 - Update rooms custom data with allowGuest. - Update system config with guest configurations.

--- migrations/development/beta.35-sql-update.sql
+++ migrations/development/beta.35-sql-update.sql
@@ -9,6 +9,12 @@ INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'gener
 INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'general/users/allowGuest', '1', 3);
 INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('server', 'players/guestUser/roleId', '2', 2);
 INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('client', 'gameEngine/banner', '0', 3);
+INSERT INTO `config` (`scope`, `path`, `value`, `type`) VALUES ('server', 'players/guestUser/allowOnRooms', '1', 3);
+
+# Rooms:
+UPDATE `rooms` SET `customData` = '{"allowGuest":true}' WHERE `name` IN ('reldens-house-1', 'reldens-town', 'reldens-forest');
+UPDATE `rooms` SET `customData` = NULL WHERE `name` = 'reldens-house-1-2d-floor';
+UPDATE `rooms` SET `customData` = '{"allowGuest":true,"gravity":[0,625],"applyGravity":true,"allowPassWallsFromBelow":true,"timeStep":0.012,"type":"TOP_DOWN_WITH_GRAVITY","useFixedWorldStep":false,"maxSubSteps":2,"movementSpeed":160,"usePathFinder":false}' WHERE `name` = 'reldens-gravity';
 
 # Rewards assets fix:
 UPDATE `objects_items_rewards_animations` SET `file` = CONCAT(`file`, '.png') WHERE `file` NOT LIKE '%.png';
