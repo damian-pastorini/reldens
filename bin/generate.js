5daf9f65dc04dfee6b78d85b0e8fefa855810b8d
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-04-28 20:37:01 +0200

    - Reldens - v4.0.0 - Generte maps commands.

--- bin/generate.js
+++ bin/generate.js
@@ -1,17 +1,61 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
 const {
     PlayersExperiencePerLevel,
     MonstersExperiencePerLevel,
     AttributesPerLevel
 } = require('@reldens/game-data-generator');
+const {
+    RandomMapGenerator,
+    LayerElementsObjectLoader,
+    LayerElementsCompositeLoader,
+    MultipleByLoaderGenerator,
+    MultipleWithAssociationsByLoaderGenerator
+} = require("@reldens/tile-map-generator");
+const { fetchFileContents } = require('./fetch-file-contents');
 
-let args = process.argv;
-if(2 === args.length){
-    console.error('- Missing arguments.', args);
-    return false;
+/**
+ *
+ * Commands:
+ *
+ * $ npx reldens-generate players-experience-per-level ./generate-data/players-experience-per-level.json
+ *
+ * $ npx reldens-generate monsters-experience-per-level ./generate-data/monsters-experience-per-level.json ./generate-data/players-level-sample.json
+ *
+ * $ npx reldens-generate attributes-per-level ./generate-data/attributes-per-level.json
+ *
+ * $ npx reldens-generate maps ./generate-data/map-data.json LayerElementsObjectLoader
+ *
+ * $ npx reldens-generate maps ./generate-data/map-composite-data.json LayerElementsCompositeLoader
+ *
+ * $ npx reldens-generate maps ./generate-data/map-composite-data-with-names.json MultipleByLoaderGenerator
+ *
+ * $ npx reldens-generate maps ./generate-data/map-composite-data-with-associations.json MultipleWithAssociationsByLoaderGenerator
+ *
+ */
+
+let mapsGenerateModes = {
+    LayerElementsObjectLoader: async (commandParams) => {
+        const loader = new LayerElementsObjectLoader(commandParams);
+        await loader.load();
+        const generator = new RandomMapGenerator(loader.mapData);
+        return await generator.generate();
+    },
+    LayerElementsCompositeLoader: async (commandParams) => {
+        const loader = new LayerElementsCompositeLoader(commandParams);
+        await loader.load();
+        const generator = new RandomMapGenerator();
+        await generator.fromElementsProvider(loader.mapData);
+        return await generator.generate();
+    },
+    MultipleByLoaderGenerator: async (commandParams) => {
+        let generator = new MultipleByLoaderGenerator({loaderData: commandParams});
+        await generator.generate();
+    },
+    MultipleWithAssociationsByLoaderGenerator: async (commandParams) => {
+        let generator = new MultipleWithAssociationsByLoaderGenerator({loaderData: commandParams});
+        await generator.generate();
+    }
 }
 
 let validCommands = {
@@ -26,35 +70,19 @@ let validCommands = {
     'attributes-per-level': (commandParams) => {
         let attributesPerLevel = new AttributesPerLevel(commandParams);
         attributesPerLevel.generate();
+    },
+    'maps': async (commandParams) => {
+        if (!mapsGenerateModes[commandParams.importMode]) {
+            console.error('- Invalid import mode. Valid options: '+Object.keys(mapsGenerateModes).join(', '));
+        }
+        mapsGenerateModes[commandParams.importMode](commandParams);
     }
 };
 
-function fetchFileContents(filePath)
-{
-    if (!filePath) {
-        console.error('- Missing data file.', filePath);
-        return false;
-    }
-
-    let relativePath = path.join(process.cwd(), filePath);
-    if (!relativePath) {
-        console.error('- Invalid data file path.', process.cwd(), filePath);
-        return false;
-    }
-
-    let fileContent = fs.readFileSync(relativePath, {encoding: 'utf8', flag:'r'});
-    if (!fileContent) {
-        console.error('- Can not read data file or file empty.', relativePath);
-        return false;
-    }
-
-    let importedJson = JSON.parse(fileContent);
-    if (!importedJson) {
-        console.error('- Can not parse data file.');
-        return false;
-    }
-
-    return importedJson;
+let args = process.argv;
+if(2 === args.length){
+    console.error('- Missing arguments.', args);
+    return false;
 }
 
 let extractedParams = args.slice(2);
@@ -70,16 +98,30 @@ if (-1 === Object.keys(validCommands).indexOf(command)) {
     return false;
 }
 
-let importedJson = fetchFileContents(extractedParams[1] || '');
+let importJson = 'monsters-experience-per-level' === command
+    || 'players-experience-per-level' === command
+    || 'attributes-per-level' === command;
 
-if ('monsters-experience-per-level' === command) {
-    let importedPlayerLevelsJson = fetchFileContents(extractedParams[2] || '');
-    if (!importedPlayerLevelsJson) {
-        console.error('- Can not parse data file for player levels.');
-        return false;
+if (importJson) {
+    let importedJson = fetchFileContents(extractedParams[1] || '');
+    if ('monsters-experience-per-level' === command) {
+        let importedPlayerLevelsJson = fetchFileContents(extractedParams[2] || '');
+        if (!importedPlayerLevelsJson) {
+            console.error('- Can not parse data file for player levels.');
+            return false;
+        }
+
+        importedJson.levelsExperienceByKey = importedPlayerLevelsJson;
     }
-
-    importedJson.levelsExperienceByKey = importedPlayerLevelsJson;
+    return validCommands[command](importedJson);
 }
 
-validCommands[command](importedJson);
+
+if ('maps' === command) {
+    return validCommands[command]({
+        rootFolder: process.cwd()+'/generate-data/',
+        mapDataFile: extractedParams[1],
+        // mapData: fetchFileContents(extractedParams[1] || ''),
+        importMode: extractedParams[2] || ''
+    });
+}
