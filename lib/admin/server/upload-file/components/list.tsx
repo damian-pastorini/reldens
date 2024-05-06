5d9e41cf7673f72e7e83e092032ff3bca11e4786
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-05-06 17:04:05 +0200

    - Reldens - v4.0.0 - Temporally dropping AdminJS in favor of importers.

--- lib/admin/server/upload-file/components/list.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import React, { FC } from 'react';
-import { ShowPropertyProps } from 'adminjs';
-
-import File from './file';
-
-const List: FC<ShowPropertyProps> = (props) => (<File width={100} {...props} />);
-
-export default List;
