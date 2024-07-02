b84d1c78f70a117ac277874dc0ac0a4f359a2e78
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-07-02 22:23:10 +0200

    - Reldens - v4.0.0 - New Admin WIP, delete from view.

--- theme/admin/reldens-admin-client.js
+++ theme/admin/reldens-admin-client.js
@@ -5,5 +5,15 @@
  */
 
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('Reldens - Administration Panel');
+    // console.log('Reldens - Administration Panel');
+    let forms = document.querySelectorAll('.form-delete');
+    if(forms){
+        for(let deleteForm of forms){
+            deleteForm.addEventListener('submit', (event) => {
+                if(!confirm('Are you sure?')){
+                    event.preventDefault();
+                }
+            });
+        }
+    }
 });
