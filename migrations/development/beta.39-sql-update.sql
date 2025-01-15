1fe0aef4b00612f659960e4588243338876dcf3c
Damian A. Pastorini <damian.pastorini@gmail.com>
2025-01-15 09:04:06 +0100

    - Reldens - v4.0.0 - Include config updates.

---
+++ migrations/development/beta.39-sql-update.sql
@@ -0,0 +1,38 @@
+--
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+--
+
+-- Config:
+INSERT INTO `config` (`scope`, `path`, `value`, `type`)
+VALUES
+    ('client', 'gameEngine/banner', '0', 3),
+    ('client', 'gameEngine/dom/createContainer', '1', 3),
+    ('client', 'gameEngine/height', '1280', 2),
+    ('client', 'gameEngine/parent', 'reldens', 1),
+    ('client', 'gameEngine/physics/arcade/debug', 'false', 3),
+    ('client', 'gameEngine/physics/arcade/gravity/x', '0', 2),
+    ('client', 'gameEngine/physics/arcade/gravity/y', '0', 2),
+    ('client', 'gameEngine/physics/default', 'arcade', 1),
+    ('client', 'gameEngine/scale/autoCenter', '1', 2),
+    ('client', 'gameEngine/scale/max/height', '1280', 2),
+    ('client', 'gameEngine/scale/max/width', '1280', 2),
+    ('client', 'gameEngine/scale/min/height', '360', 2),
+    ('client', 'gameEngine/scale/min/width', '360', 2),
+    ('client', 'gameEngine/scale/mode', '5', 2),
+    ('client', 'gameEngine/scale/parent', 'reldens', 1),
+    ('client', 'gameEngine/scale/zoom', '1', 2),
+    ('client', 'gameEngine/type', '0', 2),
+    ('client', 'gameEngine/width', '1280', 2),
+    ('client', 'general/gameEngine/updateGameSizeTimeOut', '500', 2),
+    ('client', 'ui/maximum/x', '1280', 2),
+    ('client', 'ui/maximum/y', '1280', 2),
+    ('client', 'ui/screen/responsive', '1', 3)
+ON DUPLICATE KEY UPDATE `value` = VALUES(`value`), `type` = VALUES(`type`);
+
+--
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+--
